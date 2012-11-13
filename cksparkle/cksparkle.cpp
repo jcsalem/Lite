708d0d6211c41e944ff50160ff63a33a060066e3
Jim Salem <jimdrum@gmail.com>
2012-11-13 04:56:27 -0500

    Working sparkle code

--- cksparkle/cksparkle.cpp
+++ cksparkle/cksparkle.cpp
@@ -13,6 +13,7 @@
 
 // Configuration
 Milli_t     gFrameDuration  = 40;    // duration of each frame of animation (in MS)
+float       gRate           = 1.0;  // Rate of sparkle creation
 
 // Fwd decls
 void SetupCycle(Lobj* lobj, Milli_t startTime);
@@ -23,15 +24,17 @@ void SetupCycle(Lobj* lobj, Milli_t startTime);
 void Usage(const char* progname, csref msg = "")
     {
     if (! msg.empty()) cerr << msg << endl;
-    cerr << "Usage: " << progname << CK::kStdOptionsArgs << endl;
+    cerr << "Usage: " << progname << CK::kStdOptionsArgs << " [--rate rateval]" << endl;
     cerr << "Where:" << endl;
     cerr << CK::kStdOptionsArgsDoc << endl;
+    cerr << "  rateval - Rate of sparkle creation (default is 1.0)" << endl;
     exit (EXIT_FAILURE);
     }
 
 struct option longOpts[] =
     {
         {"help",    no_argument,        0, 'h'},
+        {"rate",    required_argument,  0, 'r'},
         {0,0,0,0}
     };
 
@@ -56,6 +59,11 @@ void ParseArgs(const char* progname, int* argc, char** argv)
             {
             case 'h':
                 Usage(progname);
+            case 'r':
+                gRate = atof(optarg);
+                if (gRate <= 0)
+                    Usage(progname, "--rate argument must be positive. Was " + string(optarg));
+                break;
             default:
                 cerr << "Internal error - unknown option: " << c << endl;
                 Usage(progname);
@@ -205,6 +213,10 @@ void SparkleDim(void) {
     }
 }
 
+bool IsTimeToAlloc() {
+    return (gRate * CK::gOutputBuffer->GetCount() / 50.0) > RandomFloat(10);
+}
+
 void SparkleLoop()
 {
     Milli_t startTime = Milliseconds();
@@ -214,7 +226,7 @@ void SparkleLoop()
         gTime = Milliseconds();
         // Maybe allocate
         short num = Lobj::GetNum();
-        if (num == 0 || (num < kMaxFireflies && num < Lobj::GetMaxNum() && RandomInt(10) == 0))
+        if (num < Lobj::GetMaxNum() && IsTimeToAlloc())
           SparkleAlloc();
         SparkleDim();
         // Render
