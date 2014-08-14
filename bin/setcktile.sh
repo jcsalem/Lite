3b2fca60692818c30fbab76fae21a43406c78ee0
Jim Salem <jimdrum@gmail.com>
2014-08-14 00:00:26 -0400

    Fixed bugs in planerun. Added unix setup functions.

---
+++ bin/setcktile.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# IMPORTANT: You must source this file for it to have the desired effect
+export LDEV='ck:172.24.22.51/1(72),ck:172.24.22.51/2(72)'
+
+
---
+++ bin/setplane.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# IMPORTANT: You must source this file for it to have the desired effect
+export LDEV='plane|[flip|strip:A,strip:B]'
+
+
